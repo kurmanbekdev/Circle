f1b19560c208f76c29de7bbec060553130861a41
kurmanbekdev <kurmanbekpazylo@gmail.com>
2022-12-04 15:30:37 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle {
+    private double PI;
+    public void area(double PI){
+        double radius = PI;
+        double area = Math.PI*(radius*radius);
+        System.out.println("Айлананын аянты барабар: "+area);
+        double circumference = Math.PI*2*radius;
+        System.out.println("Айлананын узундугу барабар: "+circumference);
+    }
+
+
+    }
+
+
+
+
+
